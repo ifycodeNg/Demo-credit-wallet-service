9a7ae2bcb54d48744e4bc34282199f2347797364
IfycodeN <ifyugwumba22@gmail.com>
2022-11-18 11:00:25 +0100

    withdrawal of funds from wallet api endpoint

---
+++ test/withdrawalTest.js
@@ -0,0 +1,128 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+
+// Assertion Style
+chai.should();
+chai.use(chaiHttp);
+
+describe('Withdrawal of funds from wallet', () => {
+/**
+ * Test the Withdrawal of funds from wallet endpoint
+ */
+
+  it('Withdrawal of funds from wallet', (done) => {
+    const requestBody = {
+      amount: 100,
+      accountNumber: '0690000040',
+      narration: 'withdrawal of funds',
+      accountBank: '044',
+    };
+    chai.request(server)
+      .post('/api/v0/withdraw/funds')
+      .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImFjY291bnRJZCI6MiwiaWF0IjoxNjY4NzA1MzAzLCJleHAiOjE2NjkzMTAxMDN9.NSyFUfnpmwANaHqgGWBl5TBqmxDrNdaF5PQ3PbtAe50')
+      .send(requestBody)
+      .end((err, response) => {
+        response.should.have.status(201);
+        response.body.should.be.a('object');
+        response.body.should.have.property('success').eq(true);
+        response.body.should.have.property('msg');
+
+        done();
+      });
+  });
+
+/**
+ * Test the Withdrawal of funds from wallet with amount greater than balance endpoint
+ */
+  it('Withdrawal of funds from wallet', (done) => {
+    const requestBody = {
+      amount: 2000000,
+      accountNumber: '0690000040',
+      narration: 'withdrawal of funds',
+      accountBank: '044',
+    };
+    chai.request(server)
+      .post('/api/v0/withdraw/funds')
+      .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImFjY291bnRJZCI6MiwiaWF0IjoxNjY4NzA1MzAzLCJleHAiOjE2NjkzMTAxMDN9.NSyFUfnpmwANaHqgGWBl5TBqmxDrNdaF5PQ3PbtAe50')
+      .send(requestBody)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.should.have.property('success').eq(false);
+        response.body.should.have.property('msg');
+
+        done();
+      });
+  });
+
+  /**
+   * Test the Withdrawal of funds from wallet endpoint with incorrect body parameter
+   */
+
+  it('withdrawal would be unsuccessful', (done) => {
+    const requestBody = {
+      accountNumber: '0690000040',
+      narration: 'withdrawal of funds',
+      accountBank: '044',
+    };
+    chai.request(server)
+      .post('/api/v0/withdraw/funds')
+      .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImFjY291bnRJZCI6MiwiaWF0IjoxNjY4NzA1MzAzLCJleHAiOjE2NjkzMTAxMDN9.NSyFUfnpmwANaHqgGWBl5TBqmxDrNdaF5PQ3PbtAe50')
+      .send(requestBody)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.should.have.property('success').eq(false);
+        response.body.should.have.property('msg');
+
+        done();
+      });
+  });
+
+  /**
+   * Test the Withdrawal of funds from wallet endpoint with incorrect body key parameter
+   */
+
+  it('withdrawal would be unsuccessful', (done) => {
+    const requestBody = {
+      amount: 'abc',
+      accountNumber: '0690000040',
+      narration: 'withdrawal of funds',
+      accountBank: '044',
+    };
+    chai.request(server)
+      .post('/api/v0/withdraw/funds')
+      .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImFjY291bnRJZCI6MiwiaWF0IjoxNjY4NzA1MzAzLCJleHAiOjE2NjkzMTAxMDN9.NSyFUfnpmwANaHqgGWBl5TBqmxDrNdaF5PQ3PbtAe50')
+      .send(requestBody)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.should.have.property('success').eq(false);
+        response.body.should.have.property('msg');
+
+        done();
+      });
+  });
+
+  /**
+   * Test the fund wallet via loan endpoint without Token
+   */
+
+  it('withdrawal would be unsuccessful', (done) => {
+    const requestBody = {
+      amount: 100,
+      accountNumber: '0690000040',
+      narration: 'withdrawal of funds',
+      accountBank: '044',
+    };
+    chai.request(server)
+      .post('/api/v0/withdraw/funds')
+      .send(requestBody)
+      .end((err, response) => {
+        response.should.have.status(403);
+
+        done();
+      });
+  });
+});
